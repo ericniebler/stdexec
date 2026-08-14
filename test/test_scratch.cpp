8e312f72d341103ec7207c011eff8be341f9e328
Eric Niebler <eniebler@nvidia.com>
2026-08-14 08:17:20 -0700

    [WIP] fix the attributes of the `let_` senders

--- test/test_scratch.cpp
+++ test/test_scratch.cpp
@@ -17,6 +17,9 @@
 #include <catch2/catch_all.hpp>
 
 #include <stdexec/execution.hpp>
+
+#include <exec/sender_for.hpp>
+
 #include <test_common/receivers.hpp>
 #include <test_common/schedulers.hpp>
 
@@ -24,8 +27,25 @@ namespace ex = STDEXEC;
 
 namespace
 {
-  TEST_CASE("a scratch test case for minimal repro of a bug", "[scratch]")
+  // Return a different sender when we invoke this custom defined let_value implementation
+  struct let_value_test_domain
   {
-    CHECK(true);
+    template <exec::sender_for<ex::let_value_t> Sender>
+    static auto transform_sender(STDEXEC::set_value_t, Sender&&, auto&&...)
+    {
+      return ex::just(std::string{"hallo"});
+    }
+  };
+
+  TEST_CASE("let_value can be customized", "[adaptors][let_value]")
+  {
+    basic_inline_scheduler<let_value_test_domain> sched;
+
+    // The customization will return a different value
+    auto snd = ex::just(std::string{"hello"}) | ex::continues_on(sched)
+             | ex::let_value([](std::string& x) { return ex::just(x + ", world"); });
+    using domain_t = ex::__completion_domain_of_t<ex::set_value_t, decltype(snd), ex::__sync_wait::__env>;
+    static_assert(ex::__same_as<domain_t, let_value_test_domain>);
+    //wait_for_value(std::move(snd), std::string{"hallo"});
   }
 }  // namespace
